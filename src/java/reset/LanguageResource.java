f376ef24dafac1890931b6128fed8e39ecab7555
sebastianCano <sebastian.cano@ibadia.cat>
2016-04-20 20:53:36 +0200

    language PUT

--- src/java/reset/LanguageResource.java
+++ src/java/reset/LanguageResource.java
@@ -12,14 +12,16 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.UriInfo;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Consumes;
-import javax.ws.rs.Produces;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.PUT;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 /**
  * REST Web Service
@@ -36,10 +38,14 @@ public class LanguageResource {
     @Context
     private UriInfo context;
 
+    private String[] keys = new String[2];
+
     /**
      * Creates a new instance of LanguageResource
      */
     public LanguageResource() {
+        this.keys[0] = "name";
+        this.keys[1] = "last_update";
     }
 
     /**
@@ -82,7 +88,50 @@ public class LanguageResource {
      */
     @PUT
     @Consumes("application/json")
-    public void putJson(String content) {
+    public String putJson(String content) {
+        JSONObject json = null;
+        boolean valid = true;
+
+        //Pasar el contenido a JSON
+        try {
+            json = new JSONObject(content);
+        } catch (JSONException ex) {
+            System.out.println(ex.getMessage());
+        }
+
+        for (String key : keys) {
+            if (!json.has(key)) {
+                valid = false;
+                break;
+            }
+        }
+
+        if (json != null && valid) {
+            Connection conn = this.getConexion();
+            Statement stat;
+            int result;
+            if (conn != null) {
+                try {
+                    stat = (Statement) conn.createStatement();
+                    String query = "";
+                    try {
+                        query = "INSERT INTO language (name) VALUES ('" + json.getString("name") + "')";
+                        result = stat.executeUpdate(query);
+                    } catch (JSONException ex) {
+                        Logger.getLogger(ActorsResource.class.getName()).log(Level.SEVERE, null, ex);
+                        valid = false;
+                    }
+
+                } catch (SQLException ex) {
+                    System.out.println("language not found\n" + ex);
+                    valid = false;
+                }
+            }
+        } else {
+            System.out.println("Not valid format");
+        }
+
+        return "{'succesful' : '" + valid + "'}";
     }
 
     public Connection getConexion() {
