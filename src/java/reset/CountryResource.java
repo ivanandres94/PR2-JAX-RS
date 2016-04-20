ff1aa3d066e7672955812ad0ccb37edd6cf04625
ricardlafuente <ricard.lafuente@ibadia.cat>
2016-04-20 20:51:34 +0200

    Afegit putcountry

--- src/java/reset/CountryResource.java
+++ src/java/reset/CountryResource.java
@@ -23,21 +23,28 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PUT;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.core.MediaType;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 /**
  * REST Web Service
  *
  * @author ricard
  */
-@Path("country/{country_id}")
+@Path("country/{country_id:.*}")
 public class CountryResource {
+
     @Context
     private UriInfo context;
+
+    private String[] keys = new String[1];
 
     /**
      * Creates a new instance of actors
      */
     public CountryResource() {
+        this.keys[0] = "country";
+
     }
 
     /**
@@ -47,32 +54,29 @@ public class CountryResource {
      */
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    public String getJson(@PathParam("country_id") String country_id){
+    public String getJson(@PathParam("country_id") String country_id) {
         String Json = "";
         Connection conn = this.getConexion();
         Statement stat;
         ResultSet result;
-        if(conn!=null){
+        if (conn != null) {
             try {
                 stat = (Statement) conn.createStatement();
 
-                result = stat.executeQuery("select * from country where country_id="+country_id);
+                result = stat.executeQuery("select * from country where country_id=" + country_id);
 
                 while (result.next()) {
                     Json = "{'id ':'" + country_id + "','country ':'" + result.getString(2) + "'}";
                 }
 
             } catch (SQLException ex) {
-                System.out.println("country not found\n"+ex);
+                System.out.println("country not found\n" + ex);
             }
 
         }
-            
+
         return Json;
     }
-    
-    
-    
 
     /**
      * PUT method for updating or creating an instance of actors
@@ -81,10 +85,53 @@ public class CountryResource {
      */
     @PUT
     @Consumes(MediaType.APPLICATION_JSON)
-    public void putJson(String content) {
-    }
-    
-    public Connection getConexion(){
+    public String putJson(String content) {
+        JSONObject json = null;
+        boolean valid = true;
+
+        //Pasar el contenido a JSON
+        try {
+            json = new JSONObject(content);
+        } catch (JSONException ex) {
+            System.out.println(ex.getMessage());
+        }
+
+        for (String key : keys) {
+            if (!json.has(key)) {
+                valid = false;
+                break;
+            }
+        }
+
+        if (json != null && valid) {
+            Connection conn = this.getConexion();
+            Statement stat;
+            int result;
+            if (conn != null) {
+                try {
+                    stat = (Statement) conn.createStatement();
+                    String query = "";
+                    try {
+                        query = "INSERT INTO country (country) VALUES ('" + json.getString("country") + "')";
+                        result = stat.executeUpdate(query);
+                    } catch (JSONException ex) {
+                        Logger.getLogger(ActorsResource.class.getName()).log(Level.SEVERE, null, ex);
+                        valid = false;
+                    }
+
+                } catch (SQLException ex) {
+                    System.out.println("country not found\n" + ex);
+                    valid = false;
+                }
+            }
+        } else {
+            System.out.println("Not valid format");
+        }
+
+        return "{'succesful' : '" + valid + "'}";
+    } //{'country' : 'sabadell'}
+
+    public Connection getConexion() {
         Connection conn = null;
         try {
             Class.forName("com.mysql.jdbc.Driver").newInstance();
@@ -98,7 +145,7 @@ public class CountryResource {
         } catch (IllegalAccessException ex) {
             Logger.getLogger(ActorsResource.class.getName()).log(Level.SEVERE, null, ex);
         }
-        
+
         return conn;
     }
 }
